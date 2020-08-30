9dad5faf50fa791012dee0a9587e97c7615d18cd
Liang Chen <liang.chen@cerence.com>
2020-08-30 17:14:39 +0800

    Get a payable member for a tenant.

--- src/main/java/org/chenliang/freepark/repository/MemberRepository.java
+++ src/main/java/org/chenliang/freepark/repository/MemberRepository.java
@@ -5,6 +5,9 @@ import org.chenliang.freepark.model.Tenant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
+  Member findFirstByLastPaidAtBeforeAndTenant(LocalDate date, Tenant tenant);
 }
