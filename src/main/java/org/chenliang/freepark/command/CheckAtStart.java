d89260d05bde3fa9915e057157be66ef08f52efc
Liang Chen <slimhigh@gmail.com>
2020-08-31 21:01:53 +0800

    Create cron job to schedule checking.

--- src/main/java/org/chenliang/freepark/command/CheckAtStart.java
+++ src/main/java/org/chenliang/freepark/command/CheckAtStart.java
@@ -6,12 +6,15 @@ import org.chenliang.freepark.repository.TenantRepository;
 import org.chenliang.freepark.service.TaskManger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 @Log4j2
+@EnableScheduling
 public class CheckAtStart implements CommandLineRunner {
   @Autowired
   private TenantRepository tenantRepository;
@@ -21,6 +24,11 @@ public class CheckAtStart implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
+    scheduledCheck();
+  }
+
+  @Scheduled(cron = "${check-cron}")
+  public void scheduledCheck() {
     List<Tenant> tenants = tenantRepository.findAll();
     for (Tenant tenant : tenants) {
       taskManger.scheduleCheckTask(tenant);
