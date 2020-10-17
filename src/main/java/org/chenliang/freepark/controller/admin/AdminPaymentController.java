0595764325a49a49bcae25058c5821de98bcd21f
Chen Liang <slimhigh@gmail.com>
2020-10-17 20:42:15 +0800

    [#7] Create admin payments page API.

--- src/main/java/org/chenliang/freepark/controller/admin/AdminPaymentController.java
+++ src/main/java/org/chenliang/freepark/controller/admin/AdminPaymentController.java
@@ -1,11 +1,16 @@
 package org.chenliang.freepark.controller.admin;
 
 import org.chenliang.freepark.exception.ResourceNotFoundException;
+import org.chenliang.freepark.model.PaymentDetailResponse;
 import org.chenliang.freepark.model.PaymentResponse;
 import org.chenliang.freepark.model.entity.Tenant;
+import org.chenliang.freepark.repository.PaymentRepository;
 import org.chenliang.freepark.repository.TenantRepository;
 import org.chenliang.freepark.service.PaymentService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +23,19 @@ public class AdminPaymentController {
 
   @Autowired
   private TenantRepository tenantRepository;
+
+  @Autowired
+  private PaymentRepository paymentRepository;
+
+  @Autowired
+  private ModelMapper modelMapper;
+
+  @GetMapping("/payments")
+  public Page<PaymentDetailResponse> getPayments(Pageable pageable) {
+    return paymentRepository.findAll(pageable).map(payment ->
+        modelMapper.map(payment, PaymentDetailResponse.class)
+    );
+  }
 
   @PostMapping("/tenants/{id}/payments")
   public PaymentResponse pay(@PathVariable Integer id) {
--- src/main/java/org/chenliang/freepark/repository/PaymentRepository.java
+++ src/main/java/org/chenliang/freepark/repository/PaymentRepository.java
@@ -1,15 +1,21 @@
 package org.chenliang.freepark.repository;
 
 import org.chenliang.freepark.model.entity.Payment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+public interface PaymentRepository extends JpaRepository<Payment, Integer>, JpaSpecificationExecutor<Payment> {
   @EntityGraph(attributePaths = {"member"})
   List<Payment> getByTenantIdAndPaidAtBetween(Integer tenantId, LocalDateTime begin, LocalDateTime end);
+
+  @EntityGraph(attributePaths = {"member", "tenant"})
+  Page<Payment> findAll(Pageable pageable);
 }
