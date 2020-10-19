9b185b5a22eba5acd38cf2dc4b28908a2bf13e86
qang.zhang@nuance.com <qang.zhang@nuance.com>
2020-10-19 12:22:40 +0800

    refine email notification

--- src/main/java/org/chenliang/freepark/service/EmailService.java
+++ src/main/java/org/chenliang/freepark/service/EmailService.java
@@ -1,5 +1,6 @@
 package org.chenliang.freepark.service;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 import javax.mail.Authenticator;
 import javax.mail.Message;
@@ -10,24 +11,38 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class EmailService {
 
+  @Value("${mail.smtp.host}")
+  private String smtpHost;
+
+  @Value("${mail.smtp.port}")
+  private String smtpPort;
+
+  @Value("${mail.user}")
+  private String user;
+
+  @Value("${mail.password}")
+  private String password;
+
   public void sendMail(String to) {
-    String defaultMessage = "请缴费";
-    sendMail(to, defaultMessage);
+    String defaultSubject = "未缴费提醒";
+    String defaultContent = "请缴费";
+    sendMail(to, defaultSubject, defaultContent);
   }
 
-  public void sendMail(String sendTo, String messageContent) {
+  public void sendMail(String sendTo, String subject, String content) {
     Properties properties = System.getProperties();
     properties.setProperty("mail.smtp.auth", "true");
-    properties.setProperty("mail.smtp.host", "smtp.qq.com");
-    properties.setProperty("mail.smtp.port", "587");
-    properties.setProperty("mail.user", "532213748@qq.com");
-    properties.setProperty("mail.password", "zbkinnaeggwvbhah");
+    properties.setProperty("mail.smtp.host", smtpHost);
+    properties.setProperty("mail.smtp.port", smtpPort);
+    properties.setProperty("mail.user", user);
+    properties.setProperty("mail.password", password);
 
     Authenticator authenticator = new Authenticator() {
       @Override
@@ -41,16 +56,17 @@ public class EmailService {
     Session session = Session.getDefaultInstance(properties, authenticator);
     try {
       InternetAddress form = new InternetAddress(properties.getProperty("mail.user"));
+      form.setPersonal("free parking");
       InternetAddress to = new InternetAddress(sendTo);
 
       MimeMessage message = new MimeMessage(session);
       message.setFrom(form);
       message.addRecipient(Message.RecipientType.TO, to);
-      message.setSubject("This is the Subject Line!");
-      message.setContent("<h1>" + messageContent + "</h1>", "text/html");
+      message.setSubject(subject);
+      message.setContent("<h1>" + content + "</h1>", "text/html;charset=GB2312");
       Transport.send(message);
       log.info("Sent message successfully....");
-    } catch (MessagingException ex) {
+    } catch (MessagingException | UnsupportedEncodingException ex) {
       log.error(ex.getLocalizedMessage(), ex);
     }
   }
