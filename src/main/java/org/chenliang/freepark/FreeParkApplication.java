37c58da7a28c07e8f3de135280e65ad137b2b53c
Chen Liang <slimhigh@gmail.com>
2020-10-21 22:54:26 +0800

    Send email asynchronously.

--- src/main/java/org/chenliang/freepark/FreeParkApplication.java
+++ src/main/java/org/chenliang/freepark/FreeParkApplication.java
@@ -3,9 +3,11 @@ package org.chenliang.freepark;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableRetry
+@EnableAsync
 public class FreeParkApplication {
   public static void main(String[] args) {
     SpringApplication.run(FreeParkApplication.class, args);
