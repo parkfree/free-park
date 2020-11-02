5d47e5f982f28fad48b91d6f4406f537162c62ec
flow174 <37401102+flow174@users.noreply.github.com>
2020-11-02 14:03:16 +0800

    select the best pay member (#15)
    * select the best pay member
    
    * refine select the best pay member
    
    * Adjust the point payment logic
    
    * refactor log
    
    Co-authored-by: qang.zhang@nuance.com <qang.zhang@nuance.com>

--- src/main/java/org/chenliang/freepark/service/PaymentService.java
+++ src/main/java/org/chenliang/freepark/service/PaymentService.java
@@ -1,5 +1,13 @@
 package org.chenliang.freepark.service;
 
+import static org.chenliang.freepark.service.PaymentUtil.centToPoint;
+import static org.chenliang.freepark.service.PaymentUtil.centToYuan;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.extern.log4j.Log4j2;
 import org.chenliang.freepark.exception.ResourceNotFoundException;
 import org.chenliang.freepark.model.PaymentResponse;
@@ -19,21 +27,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static org.chenliang.freepark.service.PaymentUtil.centToPoint;
-import static org.chenliang.freepark.service.PaymentUtil.centToYuan;
-
 @Service
 @Log4j2
 public class PaymentService {
 
   @Autowired
   private RtmapService rtmapService;
+
+  @Autowired
+  private PointService pointService;
 
   @Autowired
   private MemberRepository memberRepository;
@@ -51,14 +53,14 @@ public class PaymentService {
   private EmailService emailService;
 
   private static final Map<PaymentStatus, String> STATUS_COMMENT_MAP = Map.of(
-      PaymentStatus.SUCCESS, "缴费成功",
-      PaymentStatus.NO_AVAILABLE_MEMBER, "没有可用的会员账号用于缴费",
-      PaymentStatus.CAR_NOT_FOUND, "车辆不在停车场",
-      PaymentStatus.NO_NEED_TO_PAY, "当前时段已缴费，无须再缴费",
-      PaymentStatus.NEED_WECHAT_PAY, "需要通过微信手工缴费",
-      PaymentStatus.PARK_DETAIL_API_ERROR, "调用详情API错误",
-      PaymentStatus.PAY_API_ERROR, "调用缴费API错误",
-      PaymentStatus.MEMBER_NO_DISCOUNT, "会员账号没有优惠"
+    PaymentStatus.SUCCESS, "缴费成功",
+    PaymentStatus.NO_AVAILABLE_MEMBER, "没有可用的会员账号用于缴费",
+    PaymentStatus.CAR_NOT_FOUND, "车辆不在停车场",
+    PaymentStatus.NO_NEED_TO_PAY, "当前时段已缴费，无须再缴费",
+    PaymentStatus.NEED_WECHAT_PAY, "需要通过微信手工缴费",
+    PaymentStatus.PARK_DETAIL_API_ERROR, "调用详情API错误",
+    PaymentStatus.PAY_API_ERROR, "调用缴费API错误",
+    PaymentStatus.MEMBER_NO_DISCOUNT, "会员账号没有优惠"
   );
 
   public PaymentResponse pay(int tenantId) {
@@ -68,13 +70,11 @@ public class PaymentService {
     Payment payment = new Payment();
     payment.setTenant(tenant);
 
-    LocalDate today = LocalDate.now();
-    Member member = memberRepository.findFirstPayableMember(today, tenant);
+    Member member = memberRepository.getBestPayMember(tenant);
     if (member == null) {
       log.warn("No available member for car: {}, cancel the pay schedule task", tenant.getCarNumber());
       return createResponse(payment, PaymentStatus.NO_AVAILABLE_MEMBER);
     }
-
     payment.setMember(member);
 
     ParkDetail parkDetail;
@@ -96,25 +96,22 @@ public class PaymentService {
     }
 
     ParkDetail.ParkingFee parkingFee = parkDetail.getParkingFee();
-
     if (parkingFee.getReceivable() == 0) {
       log.info("Car {} is already paid", tenant.getCarNumber());
       return createResponse(payment, PaymentStatus.NO_NEED_TO_PAY);
     }
 
-    if (parkingFee.getMemberDeductible() == 0) {
+    if (!member.isUsedToday() && parkingFee.getMemberDeductible() == 0) {
       log.info("Member {} doesn't has discount for today. Update its last paid date, and try to pay with new member",
-               member.getMobile());
-      member.setLastPaidAt(today);
+        member.getMobile());
+      member.setLastPaidAt(LocalDate.now());
       memberRepository.save(member);
       savePayment(payment, PaymentStatus.MEMBER_NO_DISCOUNT);
       return pay(tenantId);
     }
 
     payment.setAmount(parkingFee.getReceivable());
-
     int needPoints = centToPoint(parkDetail.getParkingFee().getFeeNumber());
-
     if (parkingFee.getFeeNumber() != 0 && member.getPoints() < needPoints) {
       return cannotPay(tenant, centToYuan(parkingFee.getFeeNumber()), payment);
     }
@@ -139,7 +136,11 @@ public class PaymentService {
       return createResponse(payment, PaymentStatus.PAY_API_ERROR);
     }
 
-    if (status.getCode() == 401) {
+    if (status.getCode() == 400) {
+      log.info("Pay for car {} with member {} failed cause by insufficient points, refresh points and repay", tenant.getCarNumber(), member.getMobile());
+      pointService.refreshMemberPoint(member.getId());
+      return pay(tenant.getId());
+    } else if (status.getCode() == 401) {
       log.info("Successfully paid car {} with member {}", tenant.getCarNumber(), member.getMobile());
       updateTenantTotalAmount(tenant, payment);
       updateMember(member, needPoints);
@@ -201,9 +202,9 @@ public class PaymentService {
     LocalDateTime from = today.atStartOfDay();
     LocalDateTime to = today.plusDays(1).atStartOfDay();
     return paymentRepository.getByTenantIdAndPaidAtBetween(tenant.getId(), from, to)
-        .stream()
-        .map(payment -> modelMapper.map(payment, PaymentResponse.class))
-        .collect(Collectors.toList());
+      .stream()
+      .map(payment -> modelMapper.map(payment, PaymentResponse.class))
+      .collect(Collectors.toList());
   }
 
   public Page<Payment> getPaymentsPage(Pageable pageable, PaymentSearchQuery searchQuery) {
--- src/main/java/org/chenliang/freepark/service/PointService.java
+++ src/main/java/org/chenliang/freepark/service/PointService.java
@@ -2,6 +2,7 @@ package org.chenliang.freepark.service;
 
 import lombok.extern.log4j.Log4j2;
 import org.chenliang.freepark.exception.ResourceNotFoundException;
+import org.chenliang.freepark.exception.RtmapApiRequestErrorException;
 import org.chenliang.freepark.model.entity.Member;
 import org.chenliang.freepark.model.rtmap.PointsResponse;
 import org.chenliang.freepark.repository.MemberRepository;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Log4j2
 public class PointService {
+
   @Autowired
   private MemberRepository memberRepository;
 
@@ -19,13 +21,36 @@ public class PointService {
 
   public void getPoint(int memberId) {
     Member member = memberRepository.findById(memberId)
-        .orElseThrow(() -> new ResourceNotFoundException("Member not found"));
+      .orElseThrow(() -> new ResourceNotFoundException("Member not found"));
 
     rtmapService.checkIn(member);
     PointsResponse pointsResponse = rtmapService.getAccountPoints(member);
     int oldPoints = member.getPoints();
-    member.setPoints(pointsResponse.getTotal());
-    memberRepository.save(member);
-    log.info("Update member {} points from {} to {}", member.getMobile(), oldPoints, pointsResponse.getTotal());
+    if (oldPoints != pointsResponse.getTotal()) {
+      member.setPoints(pointsResponse.getTotal());
+      memberRepository.save(member);
+      log.info("Update member {} points from {} to {}", member.getMobile(), oldPoints, pointsResponse.getTotal());
+    }
+  }
+
+  public void refreshMemberPoint(int memberId) {
+    Member member = memberRepository.findById(memberId).orElseThrow(() -> new ResourceNotFoundException("Member not found"));
+    int oldPoints = member.getPoints();
+    PointsResponse pointsResponse = null;
+    try {
+      pointsResponse = rtmapService.getAccountPoints(member);
+    } catch (RtmapApiRequestErrorException ex) {
+      member.setPoints(0);
+      log.error("Set member {} points to 0, get points failed cause by: {},", member.getMobile(), ex.getMessage(), ex);
+    }
+
+    if (pointsResponse != null) {
+      member.setPoints(pointsResponse.getTotal());
+    }
+
+    if (oldPoints != member.getPoints()) {
+      memberRepository.save(member);
+      log.info("Refresh member {} points from {} to {}", member.getMobile(), oldPoints, member.getPoints());
+    }
   }
 }
