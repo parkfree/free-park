0c396c5bfe8632e89b7c036d9059b1034701409d
Liang Chen <slimhigh@gmail.com>
2021-02-02 14:35:19 +0800

    Update schema.sql.

--- schema.sql
+++ schema.sql
@@ -1,8 +1,7 @@
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -10,76 +9,83 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `freepark` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE
+DATABASE /*!32312 IF NOT EXISTS*/ `freepark` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-USE `freepark`;
+USE
+`freepark`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `access_tokens` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `tenant_id` int NOT NULL,
-  `token` varchar(70) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `expire_at` timestamp NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `token` (`token`),
-  KEY `tenant_id` (`tenant_id`)
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `access_tokens`
+(
+    `id`         int                                    NOT NULL AUTO_INCREMENT,
+    `tenant_id`  int                                    NOT NULL,
+    `token`      varchar(70) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `expire_at`  timestamp                              NOT NULL,
+    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `token` (`token`),
+    KEY          `tenant_id` (`tenant_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `members` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `tenant_id` int DEFAULT NULL,
-  `user_id` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `open_id` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `mem_type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `mobile` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `points` int(32) NOT NULL DEFAULT '0',
-  `enable_pay` tinyint(1) NOT NULL DEFAULT '1',
-  `enable_point` tinyint(1) NOT NULL DEFAULT '0',
-  `last_paid_at` date NOT NULL DEFAULT '2020-01-01',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `tenant_id` (`tenant_id`),
-  KEY `last_paid_at` (`last_paid_at`)
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `members`
+(
+    `id`           int                                    NOT NULL AUTO_INCREMENT,
+    `tenant_id`    int                                             DEFAULT NULL,
+    `user_id`      varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `open_id`      varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `mem_type`     varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `mobile`       varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `name`         varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `points`       int                                    NOT NULL DEFAULT '0',
+    `coupons`      int                                    NOT NULL DEFAULT '0',
+    `enable_pay`   tinyint(1) NOT NULL DEFAULT '1',
+    `enable_point` tinyint(1) NOT NULL DEFAULT '0',
+    `last_paid_at` date                                   NOT NULL DEFAULT '2020-01-01',
+    `created_at`   timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`   timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY            `tenant_id` (`tenant_id`),
+    KEY            `last_paid_at` (`last_paid_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `payments` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `tenant_id` int NOT NULL,
-  `member_id` int DEFAULT NULL,
-  `amount` int NOT NULL DEFAULT '0',
-  `status` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `comment` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `paid_at` timestamp NOT NULL,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `tenant_id` (`tenant_id`),
-  KEY `member_id` (`member_id`),
-  KEY `paid_at` (`paid_at`)
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `payments`
+(
+    `id`         int                                    NOT NULL AUTO_INCREMENT,
+    `tenant_id`  int                                    NOT NULL,
+    `member_id`  int                                             DEFAULT NULL,
+    `amount`     int                                    NOT NULL DEFAULT '0',
+    `status`     varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `comment`    text COLLATE utf8mb4_unicode_ci        NOT NULL,
+    `paid_at`    timestamp                              NOT NULL,
+    `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY          `tenant_id` (`tenant_id`),
+    KEY          `member_id` (`member_id`),
+    KEY          `paid_at` (`paid_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tenants` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `car_number` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `owner` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `total_paid_amount` int NOT NULL DEFAULT '0',
-  `role` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'ROLE_USER',
-  `password` varchar(70) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `car_number` (`car_number`)
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tenants`
+(
+    `id`                int                                     NOT NULL AUTO_INCREMENT,
+    `car_number`        varchar(20) COLLATE utf8mb4_unicode_ci  NOT NULL DEFAULT '',
+    `owner`             varchar(32) COLLATE utf8mb4_unicode_ci  NOT NULL DEFAULT '',
+    `email`             varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `total_paid_amount` int                                     NOT NULL DEFAULT '0',
+    `role`              varchar(15) COLLATE utf8mb4_unicode_ci  NOT NULL DEFAULT 'ROLE_USER',
+    `password`          varchar(70) COLLATE utf8mb4_unicode_ci  NOT NULL DEFAULT '',
+    `created_at`        timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`        timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email` (`email`),
+    UNIQUE KEY `car_number` (`car_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
