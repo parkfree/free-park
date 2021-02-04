0da20b6263c851fe89cf683c17113dfd536f278e
hero0567 <hero0567@163.com>
2021-02-04 12:31:29 +0800

    user buy coupons (#19)
    * user buy coupons
    
    * add delayBound for buyCoupons
    
    * change h2 lib scope
    
    Co-authored-by: Lin Xia <lin.xia@cerence.com>

--- src/main/java/org/chenliang/freepark/scheduler/GetCouponsTaskScheduler.java
+++ src/main/java/org/chenliang/freepark/scheduler/GetCouponsTaskScheduler.java
@@ -3,12 +3,14 @@ package org.chenliang.freepark.scheduler;
 import java.time.Instant;
 import java.util.List;
 import java.util.Random;
+
 import lombok.extern.slf4j.Slf4j;
 import org.chenliang.freepark.exception.RtmapApiException;
 import org.chenliang.freepark.model.entity.Member;
 import org.chenliang.freepark.model.rtmap.ProductsResponse;
 import org.chenliang.freepark.model.rtmap.ProductsResponse.Product;
 import org.chenliang.freepark.repository.MemberRepository;
+import org.chenliang.freepark.service.CouponsService;
 import org.chenliang.freepark.service.RtmapService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,68 +22,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class GetCouponsTaskScheduler {
 
-  @Value(value = "${coupon.name}")
-  private String couponName;
+    @Autowired
+    private MemberRepository memberRepository;
 
-  @Value(value = "${coupon.number}")
-  private int couponNum;
+    @Autowired
+    private CouponsService couponsService;
 
-  @Autowired
-  private ThreadPoolTaskScheduler taskScheduler;
-
-  @Autowired
-  private MemberRepository memberRepository;
-
-  @Autowired
-  private RtmapService rtmapService;
-
-  @Scheduled(cron = "${coupon.cron}")
-  public void scheduleGetCouponsTask() {
-    final List<Member> members = memberRepository.findAll();
-    Instant now = Instant.now();
-    Random random = new Random();
-
-    final int productId = getProductId(members.get(0));
-    for (Member member : members) {
-      int delaySeconds = random.nextInt(3600 * 2);
-      Instant startTime = now.plusSeconds(delaySeconds);
-      log.info("Scheduled member {} to buy coupons at {}", member.getMobile(), startTime.toString());
-      taskScheduler.schedule(() -> {
-        try {
-          rtmapService.buy(member, productId, couponNum);
-          member.setCoupons(couponNum);
-          memberRepository.save(member);
-        } catch (RtmapApiException ignored) {
-        } catch (Exception e) {
-          log.info("Get coupons task for member {} failed with unexpected exception", member.getMobile(), e);
-        }
-      }, startTime);
-    }
-  }
-
-  private int getProductId(Member member) {
-    int startPage = 1;
-    int productId = searchCouponId(member, startPage);
-    if (productId == -1) {
-      throw new RuntimeException("Cannot found free parking coupon product");
+    @Scheduled(cron = "${coupon.cron}")
+    public void scheduleGetCouponsTask() {
+        final List<Member> members = memberRepository.findAll();
+        couponsService.buyCoupons(members, 3600 * 2);
     }
 
-    return productId;
-  }
-
-  private int searchCouponId(Member member, int page) {
-    ProductsResponse response = rtmapService.getProducts(member, page);
-    List<Product> products = response.getData().getList();
-    for (Product product : products) {
-      if (product.getMainInfo().contains(couponName)) {
-        return product.getId();
-      }
-    }
-
-    if ((page + 1) > response.getData().getPages()) {
-      return -1;
-    } else {
-      return searchCouponId(member, page + 1);
-    }
-  }
 }
