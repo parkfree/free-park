be05fd0f5952ad011b64ab1c848e67d432125530
Chen Liang <slimhigh@gmail.com>
2021-09-07 22:54:07 +0800

    Refactor: inline comparators

--- src/main/java/org/chenliang/freepark/service/MemberComparators.java
+++ src/main/java/org/chenliang/freepark/service/MemberComparators.java
@@ -6,26 +6,20 @@ import org.chenliang.freepark.service.PaymentUtil.AllocateResult;
 import java.util.Comparator;
 
 public class MemberComparators {
-  private static final Comparator<Member> compareCoupons = Comparator.comparingInt(Member::getCoupons);
-  private static final Comparator<Member> comparePoints = Comparator.comparingInt(Member::getPoints);
-
-  private static final Comparator<AllocateResult> compareAllocCoupons = Comparator.comparingInt(AllocateResult::getAllocCoupons);
-  private static final Comparator<AllocateResult> compareAllocPoints = Comparator.comparingInt(AllocateResult::getAllocPoints);
-
   public static Member minByCoupons(Member a, Member b) {
-    return minBy(a, b, compareCoupons);
+    return minBy(a, b, Comparator.comparingInt(Member::getCoupons));
   }
 
   public static Member minByPoints(Member a, Member b) {
-    return minBy(a, b, comparePoints);
+    return minBy(a, b, Comparator.comparingInt(Member::getPoints));
   }
 
   public static Member minByAllocCoupons(AllocateResult a, AllocateResult b) {
-    return minBy(a, b, compareAllocCoupons).getMember();
+    return minBy(a, b, Comparator.comparingInt(AllocateResult::getAllocCoupons)).getMember();
   }
 
   public static Member minByAllocPoints(AllocateResult a, AllocateResult b) {
-    return minBy(a, b, compareAllocPoints).getMember();
+    return minBy(a, b, Comparator.comparingInt(AllocateResult::getAllocPoints)).getMember();
   }
 
   private static <T> T minBy(T a, T b, Comparator<T> comparator) {
