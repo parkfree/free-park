f693afbf22822ba7c16a604846f0be6ff7ea1204
Chen Liang <slimhigh@gmail.com>
2022-01-18 12:19:59 +0800

    Remove unused code.

--- src/main/java/org/chenliang/freepark/FreeParkApplication.java
+++ src/main/java/org/chenliang/freepark/FreeParkApplication.java
@@ -1,6 +1,5 @@
 package org.chenliang.freepark;
 
-import org.springdoc.core.SpringDocUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,9 +8,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class FreeParkApplication {
   public static void main(String[] args) {
-//    SpringDocUtils.getConfig().replaceWithClass(org.springframework.data.domain.Pageable.class,
-//                                                org.springdoc.core.converters.models.Pageable.class);
-
     SpringApplication.run(FreeParkApplication.class, args);
   }
 }
--- src/main/java/org/chenliang/freepark/configuration/OpenApiConfig.java
+++ src/main/java/org/chenliang/freepark/configuration/OpenApiConfig.java
@@ -9,13 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@OpenAPIDefinition(info = @Info(title = "Free Park API", version = "v1"))
-//@SecurityScheme(
-//    name = "bearer token",
-//    type = SecuritySchemeType.HTTP,
-//    bearerFormat = "opaque",
-//    scheme = "bearer"
-//)
 public class OpenApiConfig {
   @Bean
   public OpenAPI customOpenAPI() {
